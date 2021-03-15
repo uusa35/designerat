6368c96dd9af97ee0d1932e48c856f6a13cbd165
uusa35 <uusa35@gmail.com>
2021-03-15 11:38:06 +0300

    updates

--- app/screens/OrderIndexScreen.js
+++ app/screens/OrderIndexScreen.js
@@ -26,7 +26,7 @@ const OrderIndexScreen = ({}) => {
   }, [orders]);
 
   return (
-    <BgContainer showImage={false}>
+    <BgContainer showImage={false} white={true}>
       <ElementsHorizontalList
         elements={currentElements}
         searchParams={{}}
